267501721e19e416c02704952b4a151473030187
pasharik <paulkryvulets@mail.ru>
2024-06-13 15:51:09 +0300

    Login system beginning

---
+++ includes/signup.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST["submit"]))
+{
+    //Grabbing the data
+    $uid = $_POST["submit"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+    $email = $_POST["email"];
+
+    //instantiate signupcontr class
+
+    //running error handler and user signup
+
+    //going back to front page
+}
--- index.php
+++ index.php
@@ -1,30 +1,73 @@
-<?php
-
-include 'D:\PHP\htdocs\website\includes\autoloader.inc.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pasha</title>
+    <title>Pasharik</title>
+    <link href="https://lntour.by/files/00997/obj/110/2523/img/d05439b02d8f265930ec1cc2ddfec988.jpg" <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <form action="includes/calc.inc.php" method="post">
-        <p>My own calculator!</p>
-        <input type="number" name="num1" placeholder="First number">
-        <select name="operator">
-            <option value="add">Addition</option>
-            <option value="sub">Subtraction</option>
-            <option value="div">Division</option>
-            <option value="mul">Multiplication</option>
-        </select>
-        <input type="number" name="num2" placeholder="Second number">
-        <button type="submit" name="submit">Calculate</button>
-    </form>
+
+    <header>
+        <nav>
+            <div>
+                <h3>LUNINETS CITY</h3>
+                <ul class="menu-main">
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">PRODUCTS</a></li>
+                    <li><a href="#">CURRENT SALES</a></li>
+                    <li><a href="#">MEMBER+</a></li>
+                </ul>
+            </div>
+            <ul class="menu-member">
+                <li><a href="#">SIGN UP</a></li>
+                <li><a href="#" class="header-login-a">LOGIN</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="index-intro">
+        <div class="index-intro-bg">
+            <div class="wrapper">
+                <div class="index-intro-c1">
+                    <div class="video"></div>
+                    <p>Один бизнесмен хотел жениться, но у него было три подружки, и он не знал какую из них выбрать. Решил их проверить: дал каждой по 1000 долларов. Одна потратила все деньги на себя, другая купила что-то для хозяйства, а третья вложила в дело и получила прибыль. Какую же из трех он выбрал? Ту, у которой грудь больше!</p>
+                </div>
+                <div class="index-intro-c2">
+                    <h2>We make<br>professional<br>gear</h2>
+                    <a href="#">FIND OUR GEAR HERE</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="index-login">
+        <div class="wrapper">
+            <div class="index-login-signup">
+                <h4>SIGN UP</h4>
+                <p>Don't have an account yet? Sign up here!</p>
+                <form action="includes/signup.inc.php" method="post">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <input type="password" name="pwdrepeat" placeholder="Repeat Password">
+                    <input type="text" name="email" placeholder="E-mail">
+                    <br>
+                    <button type="submit" name="submit">SIGN UP</button>
+                </form>
+            </div>
+        </div class="index-login-login">
+        <h4>LOGIN</h4>
+        <p>Don't have an account yet? Sign up here!</p>
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="uid" placeholder="username">
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <button type="submit" name="submit">LOGIN</button>
+        </form>
+
+    </section>
 </body>
 
 </html>
